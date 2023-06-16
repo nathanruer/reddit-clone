abb651d94c46cd0d0dc71757d82809b9c361d8fa
Nathan Ruer <nath.ruer@gmail.com>
2023-06-16 17:28:38 +0200

    ADD: leave and join subreddits

--- src/lib/validators/subreddit.ts
+++ src/lib/validators/subreddit.ts
@@ -1,12 +1,12 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const SubredditValidator = z.object({
   name: z.string().min(3).max(21),
 })
 
-export const SubRedditSubscriptionValidator = z.object({
+export const SubredditSubscriptionValidator = z.object({
   subredditId: z.string(),
 })
 
-export type CreateSubredditPayload = z.infer<typeof SubredditValidator>;
-export type SubscribeToSubredditPayload = z.infer<typeof SubRedditSubscriptionValidator>;
+export type CreateSubredditPayload = z.infer<typeof SubredditValidator>
+export type SubscribeToSubredditPayload = z.infer<typeof SubredditSubscriptionValidator>
